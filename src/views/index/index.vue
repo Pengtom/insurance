f4c1499492be40bc97d7d8ab8117551d62d06854
PRQ <1612551540@qq.com>
2024-08-14 06:31:01 +0000

    update src/views/index/index.vue.
    Signed-off-by: PRQ <1612551540@qq.com>

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -30,19 +30,31 @@
     </header>
 
     <div class="main-content">
-      <div>
-        <h1>
-          <p>
+      <div class="content-wrapper">
+        <h1 class="content-wrapper">
+          <p class="primary-text">
             专注电商需求的AI商拍工具
           </p>
-          <p>
+          <p class="secondary-text">
             鼠标就是快门 点击即看成片
           </p>
         </h1>
-        <div>打破模特、经纪、摄影、后期、场租、机酒的限制</div>
-        <div>
-          <a href="#">免费试用</a>
-          <a href="#">观看演示视频</a>
+        <div class="description">
+          打破模特、经纪、摄影、后期、场租、机酒的限制
+        </div>
+       <div class="info-sections">
+          <div class="info-item">
+            <img class="info-img-large" loading="lazy" width="91" height="106" src="https://www.weshop.com/ic_home_ai_model.webp">
+            <p class="info-text">免费试用</p>
+            <img class="info-img-small" loading="lazy" width="20" height="20" src="https://www.weshop.com/ic_home_try_arrow.svg">
+          </div>
+          <div class="info-item info-right">
+            <img class="info-img-medium" loading="lazy" width="40" height="40" src="https://www.weshop.com/ic_home_play.svg">
+            <div class="info-details">
+              <div class="info-detail-title">想了解更多?</div>
+              <div class="info-detail-item">观看演示视频</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -230,12 +242,113 @@ body, html {
   text-align: center;
   padding: 100px 20px;
   background: url('../../assets/indexImage/首页-未登录-image63.jpg') no-repeat center center/cover;
+  opacity: 0.9;
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
   color: #fff;
+}
+
+.content-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 3;
+  margin: 0;
+  padding: 10px;
+}
+
+.primary-text {
+  font-size: 24px;
+  font-weight: 500;
+  color: #fff;
+  margin: 0;
+  padding: 3px;
+}
+
+.secondary-text {
+  line-height: 56px;
+  font-size: 40px;
+  font-weight: 500;
+  margin: 0;
+  margin-top: 4px;
+}
+
+.description {
+  font-size: 16px;
+  line-height: 22px;
+  margin: 0;
+}
+
+.info-sections {
+  display: flex;
+  gap: 20px;
+  margin-top: 45px;
+}
+
+/* Styling for each info item */
+.info-item {
+  display: flex;
+  height: 64px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  color: #fff;
+  padding: 0 20px;
+  cursor: pointer;
+  font-size: 18px;
+  height: 54px;
+  min-width: 235px;
+  padding: 0 12px 0 0;
+  background-color: #7530fe;
+}
+
+/* Large image styling */
+.info-img-large {
+  width: 65px;
+  height: 76px;
+  margin-right: 12px;
+  display: block;
+  align-self: flex-end;
+}
+
+.info-text {
+  font-size: 18px;
+  margin: 0;
+  line-height: 28px;
+}
+
+.info-img-small{
+  margin-left: 4px;
+  height: 18px;
+  width: 18px;
+}
+
+.info-right{
+  background-color: hsla(0, 0%, 100%, .35);
+  min-width: 240px;
+  justify-content: flex-start;
+  padding: 7px;
+}
+
+.info-details {
+  margin-left: 8px;
+  color: #fff;
+  line-height: normal;
+}
+
+.info-detail-title {
+  font-size: 10px;
+  line-height: 14px;
+  font-weight: 500;
+}
+
+.info-detail-item {
+  font-size: 16px;
+  line-height: 25px;
+  font-weight: 500;
 }
 
 </style>
